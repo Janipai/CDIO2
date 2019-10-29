0340c2dcbd2a993125e6a64332b232ef09374930
nicklas <nicklas.manager@gmail.com>
2019-10-29 16:33:14 +0100

    Felt beskrivelser ændringer v3

--- src/GameRuleLogic.java
+++ src/GameRuleLogic.java
@@ -24,46 +24,35 @@ public class GameRuleLogic {
             case 2:
                 System.out.println("You found a fortune of "+ field[roll-2]+" points in the tower"+"\n");
                 break;
-
             case 3:
                 System.out.println("The crater collapsed on you, loosing "+ field[roll-2]+" points"+"\n");
                 break;
-
             case 4:
-
                 System.out.println("The people at the palace gates greeted you, giving you "+ field[roll-2]+" points"+"\n");
                 break;
-
             case 5:
                 System.out.println("You almost froze in the cold desert,loosing "+ field[roll-2]+" points"+"\n");
                 break;
-
             case 6:
                 System.out.println("You found a fortune of gold "+ field[roll-2]+" in the Walled city"+"\n");
                 break;
-
             case 7:
-                System.out.println("You walked into the monastery in vain, finding nothing "+ field[roll-2]+" points"+"\n");
+                System.out.println("You walked into the monastery in vain, finding nothing. "+ field[roll-2]+" points"+"\n");
                 break;
-
             case 8:
                 System.out.println("You couldn't find your way out from the black cave, loosing "+ field[roll-2]+" points"+"\n");
                 break;
-
             case 9:
                 System.out.println("You accidentally killed the fokes in the huts in the mountain, " +
-                        "and accidentally taking all of their valuables of af total "+ field[roll-2]+" points"+"\n");
+                        "and accidentally taking all of their valuables of a total of "+ field[roll-2]+" points"+"\n");
                 break;
-
             case 10:
                 System.out.println("The werewall scared you away, while "+ field[roll-2]+" points fell out of your pocket. " +
                         "You ran and gained another turn"+"\n");
                 break;
-
             case 11:
                 System.out.println("You fell into the pit,loosing "+ field[roll-2]+" points"+"\n");
                 break;
-
             case 12:
                 System.out.println("You found a jackpot of "+ field[roll-2]+" points in the goldmine"+"\n");
                 break;
@@ -75,19 +64,19 @@ public class GameRuleLogic {
         String playerinput;
 
         System.out.println(player.getName() + "'s turn" + " " + "|" + " Points: " + player.getPoints() + " " + "|" +
-                " Type \"r\" " + " to roll dices");
+                " Type \"r\"" + " to roll dices");
         playerinput = input.nextLine();
 
         if (!playerinput.equals("r")){
            System.out.println("Wrong input, try again :'(");
-            System.out.println("___________________________________________________________"+ "\n");
+            System.out.println("____________________________________________________________________________________________________________________________________________________________________________________________________________________________________________"+ "\n");
            turn(player);
        } else{
             int roll = dices.roll();
             land(roll);
             player.setPoints(field[roll-2]+player.getPoints());
             System.out.println("Accumulated points: " + player.getPoints());
-            System.out.println("___________________________________________________________"+ "\n");
+            System.out.println("____________________________________________________________________________________________________________________________________________________________________________________________________________________________________________"+ "\n");
 
        }
     }
@@ -104,6 +93,8 @@ public class GameRuleLogic {
             }
         }
     }
+
+
     private boolean player1victory(){
         if(player1.getPoints()>=3000){
             System.out.println("Congratulations, player1 won");
